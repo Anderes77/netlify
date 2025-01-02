0afe443a292093074d6c3ec24cbd60fe1d6c4429
Anderes77 <89201866+Anderes77@users.noreply.github.com>
2025-01-02 15:12:04 +0100

    Update stackbit.config.ts

--- netlify/stackbit.config.ts
+++ netlify/stackbit.config.ts
@@ -1,18 +1,19 @@
-import { defineStackbitConfig } from '@stackbit/cms-contentful';
+import { ContentfulContentSource } from '@stackbit/cms-contentful';
+import { defineStackbitConfig } from '@stackbit/types';
 
 export default defineStackbitConfig({
-    "stackbitVersion": "~0.6.0",
-    "nodeVersion": "18",
-    "ssgName": "custom",
-    "contentSources": [
-        new ContentfulContentSource({
+  stackbitVersion: '~0.6.0',
+  ssgName: 'nextjs',
+  nodeVersion: '18',
+  contentSources: [
+    new ContentfulContentSource({
       spaceId: process.env.CONTENTFUL_SPACE_ID,
       environment: process.env.CONTENTFUL_ENVIRONMENT,
       previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN,
       accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN,
     }),
-    ],
-    models: {
+  ],
+  models: {
     page: { type: 'page', urlPath: '/{slug}' },
-  }
-})
+  },
+}
